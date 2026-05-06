093fb0219bfbeb620be5c45a0013ae1101610688
george-houen <u3324971@uni.canberra.edu.au>
2026-05-06 18:09:04 +1000

    feat: add content to header and footer

--- macro_project/src/gui.py
+++ macro_project/src/gui.py
@@ -1,14 +1,37 @@
 import tkinter as tk
+import config
 
 
 root = tk.Tk()
 root.geometry("300x400")
+root.columnconfigure(0, weight=1)
+root.rowconfigure(1, weight=1)
 
 header = tk.Frame(root)
-header.grid(row=0, column=0)
+header.grid(row=0, column=0, sticky="ew")
 body = tk.Frame(root)
-body.grid(row=1, column=0)
-body = tk.Frame(root)
-body.grid(row=2, column=0)
+body.grid(row=1, column=0, sticky="ew")
+footer = tk.Frame(root)
+footer.grid(row=2, column=0, sticky="ew")
+
+#header
+header.config(relief="raised", border=2)
+
+tk.Label(header, text="Program", font=("Helvetica", 16, "bold italic")).grid(row=0, column=0)
+
+
+
+#footer 
+footer.config(relief="raised", border=2)
+footer.columnconfigure(1, weight=1)
+
+tk.Label(footer, text="u3324971").grid(row=0, column=0)
+tk.Label(footer, text="uxxxxxxx").grid(row=1, column=0)
+tk.Button(footer, text="appendix").grid(row=0, column=1, sticky="e") # TODO: currently doesnt have any function to it
+
+#body
+body.config()
+
+
 
 root.mainloop()
