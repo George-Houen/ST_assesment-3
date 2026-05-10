e6937aeaac766d44e5ec52a547e23d3da0b58b87
george-houen <u3324971@uni.canberra.edu.au>
2026-05-10 22:31:40 +1000

    fix: added size images

--- macro_project/src/gui_components.py
+++ macro_project/src/gui_components.py
@@ -15,9 +15,11 @@ class ImageLabel(Label):
     def grid(self, *args, **kwargs) -> Self:
         super().grid(*args, **kwargs)
         return self #so that we can chain the methods and i dont have to have a bunch of extra lines to grid
-    def set_image(self, image: Path):
+    def set_image(self, image: Path, size :tuple [int, int]|None = None ):
         #image_data and image_render are used from pillow to help for all image types
         #currently all images should be png, but its future proofing
         self.image_data = Image.open(image) 
+        if size:
+            self.image_data = self.image_data.resize(size, Image.Resampling.LANCZOS)
         self.image_render = ImageTk.PhotoImage(self.image_data)
         self.configure(image=self.image_render)
