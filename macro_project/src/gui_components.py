2f3c7f4cc97460878a829d15489792b865a3bb47
george-houen <u3324971@uni.canberra.edu.au>
2026-05-10 21:52:53 +1000

    feat: function to set image

--- macro_project/src/gui_components.py
+++ macro_project/src/gui_components.py
@@ -1,6 +1,7 @@
 from tkinter import Label
 from typing import Self
-
+from PIL import Image, ImageTk
+from pathlib import Path
 
 class ImageLabel(Label):
     def __init__(self, *args, **kwargs):
@@ -8,3 +9,7 @@ class ImageLabel(Label):
     def grid(self, *args, **kwargs) -> Self:
         super().grid(*args, **kwargs)
         return self #so that we can chain the methods and i dont have to have a bunch of extra lines to grid
+    def set_image(self, image: Path):
+        image_data = Image.open(image)
+        image_render = ImageTk.PhotoImage(image_data)
+        self.configure(image=image_render)
