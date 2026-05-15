5aadb6af149e441678ba846143d7cbfa3ec03171
george-houen <u3324971@uni.canberra.edu.au>
2026-05-15 13:59:45 +1000

    fix: remove rowspan

--- macro_project/src/gui.py
+++ macro_project/src/gui.py
@@ -83,7 +83,7 @@ class App(tk.Tk):
         self.data_frame: DataFrame | None
 
         self.summery_box = tk.Frame(self.page_eda, padx=3, pady=3, border=2, relief="sunken")
-        self.summery_box.grid(column=0, row=1, padx=3, pady=3, sticky="ns", rowspan=2)
+        self.summery_box.grid(column=0, row=1, padx=3, pady=3, sticky="ns")
         self.summery_box.columnconfigure([0,1], weight=1)
         
         self.class_select = ClassSelect(self.page_eda, padx=3, pady=3, border=2, relief="sunken")
