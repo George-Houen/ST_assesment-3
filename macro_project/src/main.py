5f7fbef1ae50376c44b8c376230df12cad1e434e
Owen-Johnson-07 <u3334568@uni.canberra.edu.au>
2026-05-17 19:40:22 +1000

    feat: added doc strings to all functions in gui_components.py

--- macro_project/src/main.py
+++ macro_project/src/main.py
@@ -1,14 +1,14 @@
 import config
 import services.dataset_indexer as di
 import services.eda_service as eda
-from services.workflow_service import WorkflowService
+#from services.workflow_service import WorkflowService
 import macro_project.src.gui as gui
 
-def main() -> None:
-  """Run the default non-interactive project workflow."""
-
-  workflow = WorkflowService()
-  workflow.run_full_pipeline()
+#def main() -> None:
+#  """Run the default non-interactive project workflow."""
+#
+#  workflow = WorkflowService()
+#  workflow.run_full_pipeline()
 
 if __name__ == "__main__":
   gui.App().mainloop()
